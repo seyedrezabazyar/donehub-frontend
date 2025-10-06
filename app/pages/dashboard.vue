42bf6aefcba750349c15a4e2b183758234977c0e
itmmd <mohamadnahaei.2002@gmail.com>
2025-10-06 15:18:54 +0330

    Add dashboard page

---
+++ app/pages/dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to the dashboard!</p>
+    </div>
+  </template>
+  
+  <script setup>
+  // اینجا بعدا می‌تونیم لاگیک احراز هویت اضافه کنیم
+  </script>
+  
